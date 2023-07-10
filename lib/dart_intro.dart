120ee04ba33c837ae95e0800ce5ce84c59df880b
Nanyondo mercy <nanyondomercy09@gmail.com>
2023-07-10 16:41:36 +0300

    my first dart page

---
+++ lib/dart_intro.dart
@@ -0,0 +1,17 @@
+//int calculate() {
+// return 34 * 7;
+//}
+
+void main() {
+  var name = "nanyondo";
+  String name2 = "Mercy";
+
+  print(name);
+ print(name2);
+  print(name == name2);
+  var university = "BISHOP STUART UNIVERSITY";
+  String Fullnames = "NANYONDO MERCY";
+  int age = 24;
+  
+  print("University: $university \nFullnames: $Fullnames \nAge: $age");
+}
