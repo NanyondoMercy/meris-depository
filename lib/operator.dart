48d094bb65b223963e0dd19ddc879dd596fe5b78
Nanyondo mercy <nanyondomercy09@gmail.com>
2023-07-11 12:24:01 +0300

    learn about arithmetic operators

---
+++ lib/operator.dart
@@ -0,0 +1,11 @@
+void main() {
+  // String university = "BISHOP STUART UNIVERSITY";
+  // print(university);
+  int x = 32;
+  int y = 8;
+  print("addition x + y: ${x + y}");
+  print("subtraction x - y: ${x - y}");
+  print("multiplication x * y: ${x * y}");
+  print("division x / y: ${x ~/ y}");
+  print("modulus x % y: ${x % y}");
+}
